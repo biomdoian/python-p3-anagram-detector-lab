233198c1fd1b2e50c1eeecb21ca0b8f8e7d817c0
Ian <iankruze60@gmail.com>
2025-05-14 23:07:00 +0300

    Adds the anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+     def __init__(self, target_word):
+        self.target_word = target_word
+
+     def match(self, possible_anagrams):
+        matches = []
+        target = self.target_word.lower()
+        sorted_target = sorted(target)
+
+        for word in possible_anagrams:
+            candidate = word.lower()
+            if candidate != target and sorted(candidate) == sorted(target):
+                matches.append(word)
+        return matches
